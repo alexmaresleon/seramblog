458771ffc0a379b79d1d20e1692a2d19e7ff5940
Alejandro Mares <alexmaresleon@gmail.com>
2021-11-19 18:08:12 -0600

    Tablas, relaciones y migraciones creadas

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -78,11 +78,11 @@
                         <x-slot name="content">
                             <!-- Account Management -->
                             <h6 class="dropdown-header small text-muted">
-                                {{ __('Manage Account') }}
+                                {{ __('Manejo de la cuenta') }}
                             </h6>
 
                             <x-jet-dropdown-link href="{{ route('profile.show') }}">
-                                {{ __('Profile') }}
+                                {{ __('Perfil') }}
                             </x-jet-dropdown-link>
 
                             @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
@@ -97,7 +97,7 @@
                             <x-jet-dropdown-link href="{{ route('logout') }}"
                                                  onclick="event.preventDefault();
                                                          document.getElementById('logout-form').submit();">
-                                {{ __('Log out') }}
+                                {{ __('Cerrar sesión') }}
                             </x-jet-dropdown-link>
                             <form method="POST" id="logout-form" action="{{ route('logout') }}">
                                 @csrf
